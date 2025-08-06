e997b07ee13f770f1e16538d3a112258ff7641b9
Hneumont <hberumengovea@student.neumont.edu>
2025-08-06 11:14:53 -0600

    theme support added
    added raygui default themes

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,19 +1,29 @@
 #include "libs/raylib/raylib.h"
-//#include "C://raylib/raylib/src/raymath.h"
+//#include "libs/raylib/raymath.h"
 #include "libs/raylib/rcamera.h"
+#include <string>
 #define RAYGUI_IMPLEMENTATION
 #include "libs/raygui.h"
 
 int main(void){
     const int screenWidth = 1080;
     const int screenHeight = 720;
-    bool showMessageBox = true;
+    Color bgColor = BLACK;
+    bool showMessageBox = true; //test gui implementation
+    const std::string stydir = "styles/";
+    std::string styarr[15] ={"default","lavanda","terminal","amber","ashes","bluish","candy","cherry","cyber","dark","enefete","genesis","jungle","rltech","sunny"};
+    std::string stylename = styarr[2];
+    std::string styfull = stydir+stylename+"/style_"+stylename+".rgs";
 
-    InitWindow(screenWidth, screenHeight, "TEST WINDOW");
+    InitWindow(screenWidth, screenHeight, "Test Window");
+    //loaded from filename:
+    // styles/[stylename]/[style.rgs file]
+    GuiLoadStyle(styfull.c_str());
+    //GuiLoadStyle("styles/lavanda/style_lavanda.rgs");
 
     while(!WindowShouldClose()){
         BeginDrawing();
-        ClearBackground(LIGHTGRAY);
+        ClearBackground(bgColor);
             if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
 
             if (showMessageBox)
@@ -25,4 +35,6 @@ int main(void){
             }
         EndDrawing();
     }
+    CloseWindow();
+    return 0;
 }
