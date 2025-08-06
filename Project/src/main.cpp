09b66c4e7132060e4bb122aa0d81f8cbf0ea9ba1
Hneumont <hberumengovea@student.neumont.edu>
2025-08-06 16:17:12 -0600

    Updated raygui.h and added centers
    hCenter produces horizontal center of window,
    vCenter produces vertical,
    GetScreenCenter produces (x,y) in an array [0],[1]

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,13 +1,30 @@
 #include "libs/raylib/raylib.h"
 //#include "libs/raylib/raymath.h"
 #include "libs/raylib/rcamera.h"
+#include <array>
 #include <string>
 #define RAYGUI_IMPLEMENTATION
 #include "libs/raygui.h"
 
+// Quick Center Functions
+std::array<int, 2> GetScreenCenter(){
+    int x = GetScreenWidth()/2;
+    int y = GetScreenHeight()/2;
+    std::array<int,2> pos = {x,y};
+    return pos;
+}
+int hCenter(){
+    return GetScreenWidth()/2;
+}
+int vCenter(){
+    return GetScreenHeight()/2;
+}
+// Quick Center Functions
+
 int main(void){
     const int screenWidth = 1080;
     const int screenHeight = 720;
+    //GetScreenHeight/Width return window Height/Width NOT monitor size.
     Color bgColor = BLACK;
     bool showMessageBox = true; //test gui implementation
     const std::string stydir = "styles/";
@@ -16,10 +33,10 @@ int main(void){
     std::string styfull = stydir+stylename+"/style_"+stylename+".rgs";
 
     InitWindow(screenWidth, screenHeight, "Test Window");
-    //loaded from filename:
-    // styles/[stylename]/[style.rgs file]
     GuiLoadStyle(styfull.c_str());
-    //GuiLoadStyle("styles/lavanda/style_lavanda.rgs");
+
+
+    std::string debugmsg;
 
     while(!WindowShouldClose()){
         BeginDrawing();
@@ -29,10 +46,14 @@ int main(void){
             if (showMessageBox)
             {
                 int result = GuiMessageBox((Rectangle){ 85, 70, 600, 100 },
-                    "#191#Message Box", "ewwwwwwwwwwwwwwwwwwwwwwwwwwwwww", "What the dog doin?;Megaman drops January 7th!");
+                    "#191#Message Box", "This is a message box", "Option 1/;Yes;Option 2/;No"); //; and \n work to make a new button
 
-                if (result >= 0) showMessageBox = false;
+                // result is reset to -1 every frame, thus >= is neccessary inplace of != 0
+                if (result >= 0) debugmsg = std::to_string(result); //button index -> text, index changes when a button is clicked
+                else if (result == 0) showMessageBox = false; // 0 = x Icon on top right of message box
             }
+            //DrawText(debugmsg.c_str(), screenWidth/2, screenHeight/2, 90, WHITE); //position x, position y, font-size, color
+            DrawText(debugmsg.c_str(), GetScreenCenter()[0], GetScreenCenter()[1], 90, WHITE);
         EndDrawing();
     }
     CloseWindow();
