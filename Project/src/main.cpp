54796d852430789ec5f6c3ed69c3c75510e45f5c
Hneumont <hberumengovea@student.neumont.edu>
2025-08-20 14:04:31 -0600

    basic text input
    god forgive me

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -8,6 +8,8 @@
 #define RAYGUI_IMPLEMENTATION
 #include "libs/raygui.h"
 
+//#include "keystr.h"
+
 // Quick Center Functions
 std::array<int, 2> GetScreenCenter(){
     int x = GetScreenWidth()/2;
@@ -23,7 +25,7 @@ int vCenter(){
 }
 // Quick Center Functions
 
-int* typedkeys = new int[8];
+int* typedkeys = new int[0];
 void addInput(int inInt){
     int* newarr = new int[sizeof(typedkeys)+1];
     std::copy(typedkeys, typedkeys, newarr);
@@ -71,17 +73,124 @@ int main(void){
             // result is reset to -1 every frame, thus >= is neccessary inplace of != 0
             if (result != 0 and result != -1) debugmsg = std::to_string(result); //button index -> text, index changes when a button is clicked
             else if (result == 0) WindowOpen = false; // 0 = x Icon on top right of message box
-
-            if (GetKeyPressed() <= 96 and GetKeyPressed() >= 39) { //check keycode values in raylib.h
-                //debugmsg += std::to_string(GetKeyPressed());
-                addInput(GetKeyPressed());
-            } // could be useful to make a custom text box rather than using the seemingly broken default one.
-            //position x, position y, font-size, color
-            debugmsg = "";
-            for (range-declaration : range-expression) {
-
+            if (IsKeyPressed(KEY_ENTER)) typedkeys = new int[0];
+            bool caps;
+            if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) caps = true;
+            else caps = false;
+// TEXT INPUT
+            if(IsKeyPressed(KEY_A)){
+                if(caps == true) debugmsg += "A";
+                else debugmsg += "a";
             }
-            DrawText(debugmsg.c_str(), 0, vCenter(), 90, BLACK);
+            if(IsKeyPressed(KEY_B)){
+                if(caps) debugmsg += "B";
+                else debugmsg += "b";
+            }
+            if(IsKeyPressed(KEY_C)){
+                if(caps) debugmsg += "C";
+                else debugmsg += "c";
+            }
+            if(IsKeyPressed(KEY_D)){
+                if(caps) debugmsg += "D";
+                else debugmsg += "d";
+            }
+            if(IsKeyPressed(KEY_E)){
+                if(caps) debugmsg += "E";
+                else debugmsg += "e";
+            }
+            if(IsKeyPressed(KEY_F)){
+                if(caps) debugmsg += "F";
+                else debugmsg += "f";
+            }
+            if(IsKeyPressed(KEY_G)){
+                if(caps) debugmsg += "G";
+                else debugmsg += "g";
+            }
+            if(IsKeyPressed(KEY_H)){
+                if(caps) debugmsg += "H";
+                else debugmsg += "h";
+            }
+            if(IsKeyPressed(KEY_I)){
+                if(caps) debugmsg += "I";
+                else debugmsg += "i";
+            }
+            if(IsKeyPressed(KEY_J)){
+                if(caps) debugmsg += "J";
+                else debugmsg += "j";
+            }
+            if(IsKeyPressed(KEY_K)){
+                if(caps) debugmsg += "K";
+                else debugmsg += "k";
+            }
+            if(IsKeyPressed(KEY_L)){
+                if(caps) debugmsg += "L";
+                else debugmsg += "l";
+            }
+            if(IsKeyPressed(KEY_M)){
+                if(caps) debugmsg += "M";
+                else debugmsg += "m";
+            }
+            if(IsKeyPressed(KEY_N)){
+                if(caps) debugmsg += "N";
+                else debugmsg += "n";
+            }
+            if(IsKeyPressed(KEY_O)){
+                if(caps) debugmsg += "O";
+                else debugmsg += "o";
+            }
+            if(IsKeyPressed(KEY_P)){
+                if(caps) debugmsg += "P";
+                else debugmsg += "p";
+            }
+            if(IsKeyPressed(KEY_Q)){
+                if(caps) debugmsg += "Q";
+                else debugmsg += "q";
+            }
+            if(IsKeyPressed(KEY_R)){
+                if(caps) debugmsg += "R";
+                else debugmsg += "r";
+            }
+            if(IsKeyPressed(KEY_S)){
+                if(caps) debugmsg += "S";
+                else debugmsg += "s";
+            }
+            if(IsKeyPressed(KEY_T)){
+                if(caps) debugmsg += "T";
+                else debugmsg += "t";
+            }
+            if(IsKeyPressed(KEY_U)){
+                if(caps) debugmsg += "U";
+                else debugmsg += "u";
+            }
+            if(IsKeyPressed(KEY_V)){
+                if(caps) debugmsg += "V";
+                else debugmsg += "v";
+            }
+            if(IsKeyPressed(KEY_W)){
+                if(caps) debugmsg += "W";
+                else debugmsg += "w";
+            }
+            if(IsKeyPressed(KEY_X)){
+                if(caps) debugmsg += "X";
+                else debugmsg += "x";
+            }
+            if(IsKeyPressed(KEY_Y)){
+                if(caps) debugmsg += "Y";
+                else debugmsg += "y";
+            }
+            if(IsKeyPressed(KEY_Z)){
+                if(caps) debugmsg += "Z";
+                else debugmsg += "z";
+            }
+        // Special Input
+            if(IsKeyPressed(KEY_ENTER)){
+                debugmsg += '\n';
+            }
+            if (IsKeyPressed(KEY_BACKSPACE)){
+                if (!debugmsg.empty()) debugmsg.erase(debugmsg.size()-1);
+            }
+// TEXT INPUT END
+            DrawText(debugmsg.c_str(), 0, 75, 90, BLACK);
         EndDrawing();
         if (WindowShouldClose()) WindowOpen = false;
     }
